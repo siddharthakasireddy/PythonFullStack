e5a20200a2df77f5c77dccc18cd82d1e777f38fb
Siddharthakasireddy <siddharthakasireddy8@gmail.com>
2026-08-06 23:00:34 +0530

    Day 2and3: Completed today's Python programs

---
+++ Python/Day02.py
@@ -0,0 +1,31 @@
+#Statements
+x = 10   #Assignment statement
+print(x) #Function call statement
+if x > 5:  #Conditional statement
+    print("x is big")
+    
+#Identifiers
+name = "Alice"  #Variable name
+age = 25
+def greet():
+    print("Hello!")
+    
+#Comments
+# This is a single-line comment
+"""
+This is a multi-line comment
+"""
+
+#swapping two numbers
+a = 5
+b = 10
+# Using a temporary variable
+temp = a
+a = b
+b = temp
+print(a, b)
+
+#Deleting a variable
+x = 10
+del x
+print(x)  # This will raise an NameError since x is deleted
---
+++ Python/Day03.py
@@ -0,0 +1,26 @@
+#Arithmetic Operator
+a = 10
+b = 20
+
+a = a + b
+b = a - b
+a = a - b
+
+print(a, b)
+
+#Types of Datatypes
+x = 100 #Int
+
+print(x)
+print(type(x))
+
+price = 199.99 #Float
+
+print(type(price))
+
+#List
+numbers = [10, 20, 30]
+
+numbers.append(40)
+
+print(numbers)
