edf42f466757871312c450cdad76b269259f4bbb
Siddharthakasireddy <siddharthakasireddy8@gmail.com>
2026-08-08 22:57:07 +0530

    Practice

---
+++ Python/Day08.py
@@ -0,0 +1,43 @@
+#List Operations
+a = [1, 2]
+b = [3, 4]
+print(a + b)
+
+print([1, 2] * 3)
+
+data = [10, 20, 30, 40]
+print(data[0])
+print(data[-1])
+
+data = [10, 20, 30, 40, 50]
+print(data[1:4])
+print(data[::-1])
+
+data = [10, 20, 30]
+print(20 in data)
+print(100 not in data)
+
+#Nested Lists
+data = [[1, 2], [3, 4]]
+print(data[0])
+print(data[1][1])
+
+#Tuple operations
+a = (1, 2)
+b = (3, 4)
+print(a + b)
+
+
+print((1, 2) * 3)
+
+data = (10, 20, 30, 40)
+print(data[0])
+print(data[-1])
+
+data = (10, 20, 30, 40, 50)
+print(data[1:4])
+print(data[::-1])
+
+data = (10, 20, 30)
+print(20 in data)
+print(100 not in data)
