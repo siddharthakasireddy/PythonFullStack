b678c25e9908e370c888dd3c74aa79b49ff58e44
Siddharthakasireddy <siddharthakasireddy8@gmail.com>
2026-08-17 20:51:32 +0530

    Recursion

---
+++ Python/Day17.py
@@ -0,0 +1,35 @@
+def factorial(n):
+    if n == 0 or n == 1:       # Base case
+        return 1
+    else:
+        return n * factorial(n - 1)  # Recursive case
+
+
+n = int(input("Enter a number: "))
+print("Factorial:", factorial(n))
+
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+n = int(input("Enter number of terms: "))
+
+for i in range(n):
+    print(fibonacci(i), end=" ")
+    
+
+    
+def sum_natural(n):
+    if n == 1:
+        return 1
+    else:
+        return n + sum_natural(n - 1)
+
+n = int(input("Enter n value: "))
+
