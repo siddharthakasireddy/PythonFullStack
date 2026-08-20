c78f9baec2de3475ec521adad0fa70fe7a68b800
Siddharthakasireddy <siddharthakasireddy8@gmail.com>
2026-08-20 22:28:16 +0530

    Files

---
+++ Python/Day21.py
@@ -0,0 +1,14 @@
+path = r"C:\Users\siddh\Downloads\pfs64batch.txt"
+f = open(path, "a")
+#f.write("Hello World")
+#f.close()
+#print("File written successfully.")
+
+#l = ['line 1\n', 'line 2\n', 'line 3\n']
+#f.writelines(l)
+f#.close()
+#print("File written successfully.")
+
+print(f.write("I'm from Hyderabad"))
+f.close()
+print("File read successfully.")
